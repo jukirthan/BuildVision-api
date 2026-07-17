0d22d9835b19fd67637fb2b69984296ea8faf14a
Local User <local@example.com>
2026-07-17 11:43:02 +0530

    run.py modify

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
+import os
+
 from app import create_app
 
-app = create_app()
+config_name = os.getenv("FLASK_ENV", "production")
+app = create_app(config_name)
 
 if __name__ == "__main__":
-  app.run(host="127.0.0.1", port=5000, debug=True)
+  port = int(os.getenv("PORT", "5000"))
+  app.run(host="0.0.0.0", port=port, debug=config_name == "development")
